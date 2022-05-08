79fcc902a65f9ba049ab2b62cc9cfa8a6c53e470
Taye Lemma <lemmataye@yahoo.com>
2022-05-08 01:37:40 -0500

    Lab4

--- Lab4/exercise1.js
+++ Lab4/exercise1.js
@@ -1,5 +1,5 @@
 /**
- * Implement Factory Method pattern to create two types of light bulbs: regular bulbs and energy saver bulbs.
+ * Implement Factory-Method-pattern to create two types of light bulbs: regular bulbs and energy saver bulbs.
     Regular bulbs have a range of lumens between 50 and 100 and last for 1 year.
     Energy saver bulbs have a range of lumens between 5 and 40 and last for 10 years and comes in multiple colors.
     When you finish, you can use the code below to test:
